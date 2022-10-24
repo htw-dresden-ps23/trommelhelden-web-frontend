6fbe2efe55e34bcbed08a51f195f9b550e327525
Fabian Krähmer <s82628@informatik.htw-dresden.de>
2022-10-24 15:26:57 +0200

    Docker Compose

--- Docker/setup_database.sh
+++ Docker/setup_database.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Wait for database to startup 
 sleep 20
-./opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Trommelh€ld€n -i start.sql
+./opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Tromm3lh€ld€n -i start.sql
