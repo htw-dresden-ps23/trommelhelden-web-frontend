191ec8633ca4d638a735b5ee6b8ff5139a266530
Klemens Trägre <traegerklemens@gmail.com>
2022-10-31 20:16:19 +0100

    feat(frontend): :construction: added new routes

---
+++ frontend/src/router/routes.ts
@@ -0,0 +1,42 @@
+export const routes = [
+  {
+    path: "/orders",
+    component: () => import("@/views/Orders/OrderOverview.vue"),
+  },
+  {
+    // UserProfile will be rendered inside User's <router-view>
+    // when /user/:id/profile is matched
+    path: "/orders/create",
+    component: () => import("@/views/Orders/CreateOrder.vue"),
+  },
+  {
+    // UserProfile will be rendered inside User's <router-view>
+    // when /user/:id/profile is matched
+    path: "/orders/plan",
+    component: () => import("@/views/Orders/PlanOrder.vue"),
+  },
+  {
+    path: "/",
+    redirect: "/dashboard",
+  },
+  {
+    path: "/dashboard",
+    component: () => import("@/views/Dashboard.vue"),
+  },
+  {
+    path: "/employees",
+    component: () => import("@/views/MasterData/Employees.vue"),
+  },
+  {
+    path: "/customers",
+    component: () => import("@/views/MasterData/Customers.vue"),
+  },
+  {
+    path: "/products",
+    component: () => import("@/views/MasterData/Customers.vue"),
+  },
+  {
+    path: "/orders/complete",
+    component: () => import("@/views/Orders/CompleteOrder.vue"),
+  },
+];
