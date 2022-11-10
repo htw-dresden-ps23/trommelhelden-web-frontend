870f7d3dfa9d9515ddc86288cee2d3f3f19494c9
Lukas Möbius <s80475@htw-dresden.de>
2022-11-10 15:06:38 +0100

    backend routes angepasst

--- backend/src/middleware/index.ts
+++ backend/src/middleware/index.ts
@@ -2,15 +2,26 @@ import express, { NextFunction, Request, Response } from "express";
 import { Prisma } from "@prisma/client";
 
 export const prismaErrorMiddleware = (error: any, req: Request, res: Response, next: NextFunction) => {
+
+    if (! error) {
+        return next();
+    }
+  
   if (error instanceof Prisma.PrismaClientValidationError) {
+    console.log("Error Validation" ,error.message);
     return res.status(400).json({ error: error.message });
   }
   if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    console.log("Error PrismaClientKnownRequestError" ,error.message);
     return res.status(400).json({ error: error.message, code: error.code, meta: error.meta });
   }
   if (error instanceof Prisma.PrismaClientUnknownRequestError) {
+    console.log("Error PrismaClientUnknownRequestError" ,error.message);
     return res.status(400).json({ error: error.message });
   }
 
-  next();
+
+
+    res.status(500);
+    res.send('500: Internal server error');
 };
