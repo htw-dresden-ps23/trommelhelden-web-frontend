ff252244ee0605c4cf7bb12efbf23e76cf0c7e0e
Alexander Pronin <aleksandr.pronin@htw-dresden.de>
2022-11-14 21:30:22 +0100

    Dashboard pilot

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,9 +1,80 @@
-<template>
+<!-- <template>
   <div>
     <div>Test</div>
   </div>
 </template>
+ -->
 
-<script setup lang="ts"></script>
+
+<template>
+  <div>
+      <Chart type="bar" :data="chartData" :options="chartOptions" />
+      yo
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import {  ref } from "vue";
+
+
+let chartData:any = ref({});
+
+ chartData = {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    type: 'line',
+                    label: 'Dataset 1',
+                    borderColor: '#42A5F5',
+                    borderWidth: 2,
+                    fill: false,
+                    data: [50,25,12,48,56,76,42]
+                }, {
+                    type: 'bar',
+                    label: 'Dataset 2',
+                    backgroundColor: '#66BB6A',
+                    data: [21,84,24,75,37,65,34],
+                    borderColor: 'white',
+                    borderWidth: 2
+                }, {
+                    type: 'bar',
+                    label: 'Dataset 3',
+                    backgroundColor: '#FFA726',
+                    data: [41,52,24,74,23,21,32]
+                }]
+            }
+
+            const chartOptions = {
+                plugins: {
+                    legend: {
+                        labels: {
+                            color: '#495057'
+                        }
+                    }
+                },
+                scales: {
+                    x: {
+                        ticks: {
+                            color: '#495057'
+                        },
+                        grid: {
+                            color: '#ebedef'
+                        }
+                    },
+                    y: {
+                        ticks: {
+                            color: '#495057'
+                        },
+                        grid: {
+                            color: '#ebedef'
+                        }
+                    }
+                }
+              }
+
+</script>
 
 <style></style>
+
+
+
