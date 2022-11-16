4c3deda05ca60b5d22789873f8c2187f01d084d5
Alexander Pronin <aleksandr.pronin@htw-dresden.de>
2022-11-16 19:52:04 +0100

    dashboard

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,80 +1,46 @@
-<!-- <template>
-  <div>
-    <div>Test</div>
-  </div>
-</template>
- -->
-
-
 <template>
-  <div>
-      <Chart type="bar" :data="chartData" :options="chartOptions" />
-      yo
-  </div>
-</template>
+ <BarChart v-bind="barChartProps" style="padding-right:10rem"/>
+ <DoughnutChart :chartData="chartData"  style="padding-right:10rem"/>
+ <LineChart :chartData="chartData"/>
 
-<script setup lang="ts">
+ </template>
+  
+<script lang="ts">
+import { ref, computed, defineComponent } from 'vue';
+import { Chart, registerables } from 'chart.js';
+import { BarChart, useBarChart, DoughnutChart, LineChart } from 'vue-chart-3';
 
-import {  ref } from "vue";
 
+Chart.register(...registerables);
 
-let chartData:any = ref({});
+export default defineComponent({
+    name: 'DashboardTH',
+    components: { DoughnutChart, BarChart, LineChart },
+    setup() {
+        const data = ref([30, 40, 60, 70, 5]);
 
- chartData = {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                datasets: [{
-                    type: 'line',
-                    label: 'Dataset 1',
-                    borderColor: '#42A5F5',
-                    borderWidth: 2,
-                    fill: false,
-                    data: [50,25,12,48,56,76,42]
-                }, {
-                    type: 'bar',
-                    label: 'Dataset 2',
-                    backgroundColor: '#66BB6A',
-                    data: [21,84,24,75,37,65,34],
-                    borderColor: 'white',
-                    borderWidth: 2
-                }, {
-                    type: 'bar',
-                    label: 'Dataset 3',
-                    backgroundColor: '#FFA726',
-                    data: [41,52,24,74,23,21,32]
-                }]
-            }
-
-            const chartOptions = {
-                plugins: {
-                    legend: {
-                        labels: {
-                            color: '#495057'
-                        }
-                    }
+        const chartData = computed(() => ({
+            labels: ['Mitarbeiter', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
+            datasets: [
+                {
+                    data: data.value,
+                    backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED'],
                 },
-                scales: {
-                    x: {
-                        ticks: {
-                            color: '#495057'
-                        },
-                        grid: {
-                            color: '#ebedef'
-                        }
-                    },
-                    y: {
-                        ticks: {
-                            color: '#495057'
-                        },
-                        grid: {
-                            color: '#ebedef'
-                        }
-                    }
-                }
-              }
+            ],
+        }));
 
+
+        const bubbleChartData = computed(() => ({
+            labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
+            
+        }));
+
+        const { barChartProps, barChartRef } = useBarChart({
+            chartData,
+        });
+
+
+        return { barChartProps, barChartRef, chartData,bubbleChartData };
+    },
+});
 </script>
-
-<style></style>
-
-
-
