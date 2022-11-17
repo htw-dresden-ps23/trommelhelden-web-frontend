5ebc6856a68a32d6e02697b6210fb5d5f6e35191
Alexander Pronin <aleksandr.pronin@htw-dresden.de>
2022-11-17 14:30:05 +0100

    Invoices

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -3,9 +3,55 @@
  <DoughnutChart :chartData="chartData"  style="padding-right:10rem"/>
  <LineChart :chartData="chartData"/>
 
+<!-- 
+ <div class="grid grid-cols-1 gap-6 lg:grid-cols-3 mb-6">
+        <CardBoxWidget
+          trend="12%"
+          trend-type="up"
+          color="text-emerald-500"
+          :icon="mdiAccountMultiple"
+          :number="512"
+          label="Clients"
+        />
+        <CardBoxWidget
+          trend="12%"
+          trend-type="down"
+          color="text-blue-500"
+          :icon="mdiCartOutline"
+          :number="7770"
+          prefix="$"
+          label="Sales"
+        />
+        <CardBoxWidget
+          trend="Overflow"
+          trend-type="alert"
+          color="text-red-500"
+          :icon="mdiChartTimelineVariant"
+          :number="256"
+          suffix="%"
+          label="Performance"
+        />
+      </div>
+
+  -->
+
  </template>
   
 <script lang="ts">
+
+// import {
+//   mdiAccountMultiple,
+//   mdiCartOutline,
+//   mdiChartTimelineVariant,
+//   mdiMonitorCellphone,
+//   mdiReload,
+//   mdiGithub,
+//   mdiChartPie,
+// } from "@mdi/js";
+
+
+
+
 import { ref, computed, defineComponent } from 'vue';
 import { Chart, registerables } from 'chart.js';
 import { BarChart, useBarChart, DoughnutChart, LineChart } from 'vue-chart-3';
--- frontend/src/views/Invoices.vue
+++ frontend/src/views/Invoices.vue
@@ -1,9 +1,83 @@
 <template>
-  <div><EntityTable></EntityTable></div>
+    <div class="my-8 h-fit">
+    <div class="h-full rounded-xl bg-white px-6 py-8 shadow-2xl">
+      <div class="flex items-center justify-between">
+        <h1
+          class="bg-gradient-to-r from-blue-400 to-pink-800 bg-clip-text py-4 text-6xl font-extrabold text-transparent"
+        >
+          Rechungen
+        </h1>
+       
+      </div>
+      <Divider />
+    <EntityTable
+        :key="reRender"
+        name="Rechnungen"
+        primary-key="KunNr|AufNr"
+        :show-rows="5"
+        :columns="columns"
+        resource-name="invoices"
+        :allow-edit="false"
+        :allow-delete="false"
+        :opt-params="{ status: 'planned' }"
+        :emit-edit-only="true"
+        @edit-row="onButtonClick"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import EntityTable from "@/components/Entity/EntityTable.vue";
+import { useDialog } from "primevue/usedialog";
+import { inject, onMounted, ref } from "vue";
+import { IRechnung } from "@/types";
+import CompleteOrderForm from "@/components/Order/CompleteOrderForm.vue";
+
+
+const reRender = ref(0);
+const dialog = useDialog();
+
+
+const columns = [
+  {
+    label: "Datum",
+    name: "RechDat",
+    type: "date",
+  },
+  {
+    label: "Betrag",
+    name: "RechBetrag",
+    type: "numeric",
+  },
+  {
+    label: "KunNr",
+    name: "KunNr",
+    type: "numeric",
+  },
+  {
+    label: "Kunde.KunName",
+    name: "Kunde",
+    type: "relation",
+  },
+  {
+    label: "AufNr",
+    name: "AufNr",
+    type: "numeric",
+  },
+  {
+    label: "Auftrag",
+    name: "Auftrag",
+    type: "relation",
+  },
+];
+
+
+const onButtonClick = (order: IRechnung) => {
+  console.log(order);
+};
+
+
 </script>
 
 <style scoped></style>
