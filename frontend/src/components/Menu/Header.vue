7dd9a2be4d4399ec78a5f5a9b052d106cf679446
Alexander Pronin <aleksandr.pronin@htw-dresden.de>
2022-11-27 20:40:26 +0100

    Button "Settings" in Header

--- frontend/src/components/Menu/Header.vue
+++ frontend/src/components/Menu/Header.vue
@@ -3,13 +3,17 @@
     <div
       class="flex h-10 items-center bg-gradient-to-r from-blue-400 to-pink-600 shadow"
     >
-      <div class="m-4 mr-auto flex">
+      <div class="headerbar m-4 mr-auto flex justify-between" style="width: 98%">
         <Button
           icon="pi pi-align-justify"
           icon-class="text-[#f6f6f6]"
           class="p-button-rounded p-button-text p-button-plain"
           @click="store.sideBarMenuIsOpen = !store.sideBarMenuIsOpen"
         />
+        <Button
+        class="p-button-rounded p-button-lg  p-button-text !text-slate-100"
+        icon="pi pi-cog"
+      />
       </div>
     </div>
   </header>
