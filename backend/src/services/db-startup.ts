abbc96788f686e8b1b69641e173816d9e408d64d
Paulus Tamm <109551602+ptamm2022@users.noreply.github.com>
2023-06-09 11:57:55 +0200

    Zufallsdaten an Domäne angepasst

--- backend/src/services/db-startup.ts
+++ backend/src/services/db-startup.ts
@@ -11,26 +11,63 @@ import {
   prisma,
 } from "@prisma/client";
 
+
 function createRandomErsatzteil(): Prisma.ErsatzteilCreateManyInput {
-  const EtID = faker.helpers.unique(faker.random.alphaNumeric, [
-    5,
-    {
-      casing: "upper",
-    },
-  ]);
+    const EtID = faker.helpers.unique(faker.random.alphaNumeric, [
+        5,
+        {
+          casing: "upper",
+        },
+    ]);
 
-  const EtBezeichnung = faker.helpers.unique(faker.commerce.productName);
-  const EtPreis = faker.datatype.number({ min: 0.1, max: 30, precision: 0.01 });
-  const EtAnzLager = faker.datatype.number({ min: 0, max: 1000 });
-  const EtHersteller = faker.company.name().slice(0, 30);
+    const ersatzteilNamen = [
+        "Anschlussschlauch",
+        "Antriebsriemen",
+        "Einsatz für Einspülschale",
+        "Einspülschale",
+        "Einspülschalenunterteil",
+        "Federkolbendämpfer mit Befestigungssatz",
+        "Flusensieb",
+        "Flusensiebeinsatz",
+        "Heizelement",
+        "Kohlebürsten",
+        "Kugellager",
+        "Lagersatz",
+        "Laugenpumpe",
+        "Magnetventil",
+        "Pumpensumpf Ablaufschlauch",
+        "Saugschlauch",
+        "Stoßdämpfer",
+        "Temperaturfühler",
+        "Türverriegelung elektrisch",
+        "Verriegelungsrelais"
+    ];
 
-  return {
-    EtID,
-    EtBezeichnung,
-    EtPreis,
-    EtAnzLager,
-    EtHersteller,
-  };
+    const zufälligerIndexErsatzteil = Math.floor(Math.random() * ersatzteilNamen.length);
+    const EtBezeichnung = ersatzteilNamen[zufälligerIndexErsatzteil];
+    // const EtBezeichnung = faker.helpers.unique(faker.commerce.productName);
+    const EtPreis = faker.datatype.number({ min: 2, max: 70, precision: 0.01 });
+    const EtAnzLager = faker.datatype.number({ min: 0, max: 400 });
+
+    const herstellerNamen = [
+        "Hauknecht",
+        "Liemens",
+        "Mosch",
+        "Tiele"
+    ];
+    
+    const zufälligerIndexHersteller = Math.floor(Math.random() * herstellerNamen.length);
+    const EtHersteller = herstellerNamen[zufälligerIndexHersteller];
+
+    // const EtHersteller = faker.company.name().slice(0, 30);
+
+    return {
+      EtID,
+      EtBezeichnung,
+      EtPreis,
+      EtAnzLager,
+      EtHersteller,
+    };
 }
 
 function createRandomNiederlassung(i: number): Prisma.NiederlassungCreateManyInput {
@@ -63,8 +100,20 @@ function createRandomMitarbeiter(
   const MitName = faker.name.lastName();
   const MitVorname = faker.name.firstName();
   const MitGebDat = faker.date.between("1950-01-01", "2000-12-31");
-  const MitJob = faker.name.jobType();
-  const MitStundensatz = faker.datatype.number({ min: 12.5, max: 50 });
+
+  const jobNamen = [
+    "Auftragskoordinator",
+    "Einkauf",
+    "Lagermitarbeiter",
+    "Meister",
+    "Monteur"
+  ];
+
+  const zufälligerIndexJob = Math.floor(Math.random() * jobNamen.length);
+  const MitJob = jobNamen[zufälligerIndexJob];
+
+  // const MitJob = faker.name.jobType();
+  const MitStundensatz = faker.datatype.number({ min: 75, max: 125 });
   const NLNr = faker.helpers.arrayElement(ret).NLNr;
 
   return {
@@ -89,7 +138,35 @@ function createRandomAuftrag(
 ): Prisma.AuftragCreateInput {
   const MitID = faker.helpers.arrayElement(mitarbeiter).MitID;
   const KunNr = faker.helpers.arrayElement(kunden).KunNr;
-  const Beschreibung = faker.company.bs();
+  
+  const beschreibungAuftrag = [
+    "Dichtung porös",
+    "Feinjustierung Axialmotor",
+    "häufige Überladung",
+    "Hauptlager ausgeschlagen",
+    "Heizstäbe verkalkt",
+    "instabiler Aufstellort",
+    "Kabelbruch",
+    "Laugenpumpe leckt",
+    "Leckage am Wasserkreislauf",
+    "Reinigung Vorfilter",
+    "Sondermodell",
+    "Steckerleister korrodiert",
+    "Trommel klappert",
+    "Vollverkalkung",
+    "Wackelkontakt Drehschalter",
+    "Wackelkontakt Steckerleiste"
+  ];
+
+  var Beschreibung = "";
+
+  if (Math.random() <= 0.03) {
+    const zufälligerIndexBeschreibung = Math.floor(Math.random() * beschreibungAuftrag.length);
+    Beschreibung = beschreibungAuftrag[zufälligerIndexBeschreibung];
+  } 
+
+  // const Beschreibung = faker.company.bs();
+  
   const ErlDat = faker.date.between(
     new Date().setFullYear(2015),
     new Date().setFullYear(2023)
